3210548a1625373539807863b5366572b0e6f51e
Diego Fidalgo <diego-fidalgo@hotmail.com>
2021-12-08 04:40:02 -0300

    Fix: exemplo mdc

--- examples/mdc.c
+++ examples/mdc.c
@@ -1,13 +1,15 @@
 /* Um programa para calcular mdc */
-int gdc (int u, int v)
-{
-   if (v == 0) return u;
-   else return gdc(v,u-u/v*v);
-   /* u-u / v*v == u mod v */
+int gdc(int u, int v) {
+    if (v == 0)
+        return u;
+    else
+        return gdc(v, u - u / v * v);
+    /* u-u / v*v == u mod v */
 }
 
-void main(void)
-{
-   int r[2];
-   gdc(r[0]);
+int main(void) {
+    int r[2];
+    r[0] = 10;
+    r[1] = 5;
+    return gdc(r[0], r[1]);
 }
